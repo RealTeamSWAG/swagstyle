546847e55bfb466ef8fa1c06d5392054a2885810
Stan Callewaert <callewaert.stan@gmail.com>
2015-10-27 17:41:52 +0100

    Changed time of event to 12 pm

--- js/time.js
+++ js/time.js
@@ -1,3 +1,3 @@
-$('#time').countdown('2016/02/29', function(event) {
+$('#time').countdown('2016/02/29 12:00:00', function(event) {
     $(this).html(event.strftime('<span>%D</span> days     <span>%H</span> hours     <span>%M</span> minutes'));
 });
